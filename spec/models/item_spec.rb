758bd28d479742926a86c7eb2fe3c8c3fdb2ebe5
Kota Ishida <kota.ishida@di-v.co.jp>
2023-10-20 01:58:21 +0900

    All Actions

---
+++ spec/models/item_spec.rb
@@ -0,0 +1,93 @@
+require 'rails_helper'
+
+RSpec.describe Item, type: :model do
+  before do
+    @item = FactoryBot.build(:item)
+  end
+
+  describe '商品出品' do
+    context '商品出品がうまくいくとき' do
+      it '全ての項目が存在すれば登録できる' do
+        expect(@item).to be_valid
+      end
+    end
+
+    context '新規出品できない場合' do
+      it '商品画像が空では登録できない' do
+        @item.image = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Image can't be blank")
+      end
+
+      it '商品名が空では登録できない' do
+        @item.item_name = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Item name can't be blank")
+      end
+
+      it '商品の説明が空では登録できない' do
+        @item.description = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Description can't be blank")
+      end
+
+      it '価格の情報が空では登録できない' do
+        @item.price = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price can't be blank")
+      end
+
+      it '価格が300未満では登録できない' do
+        @item.price = 299
+        @item.valid?
+        expect(@item.errors.full_messages).to include('Price must be greater than or equal to 300')
+      end
+
+      it '価格が9,999,999より大きいと登録できない' do
+        @item.price = 10_000_000
+        @item.valid?
+        expect(@item.errors.full_messages).to include('Price must be less than or equal to 9999999')
+      end
+      it '価格が半角数字以外を含む場合は登録できない' do
+        @item.price = '１０００'
+        @item.valid?
+        expect(@item.errors.full_messages).to include('Price is not a number')
+      end
+      it 'ユーザーが紐付いていなければ投稿できない' do
+        @item.user = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include('User must exist')
+      end
+      it 'カテゴリーが未選択の場合' do
+        @item.category_id = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include('Category must be other than 1')
+      end
+
+      it '商品の状態が未選択の場合' do
+        @item.condition_id = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include('Condition must be other than 1')
+      end
+
+      it '配送料の負担が未選択の場合' do
+        @item.delivery_fee_payment_id = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include('Delivery fee payment must be other than 1')
+      end
+
+      it '発送元の地域が未選択の場合' do
+        @item.region_id = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include('Region must be other than 1')
+      end
+
+      it '発送までの日数が未選択の場合' do
+        @item.deadline_id = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include('Deadline must be other than 1')
+      end
+
+    end
+  end
+end
